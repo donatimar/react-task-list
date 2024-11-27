7337c7db2ae29dac655ad6ff56e0ed2b798560fc
donatimar <donatimar@gmail.com>
2024-11-27 13:48:03 +0100

    created file tasks and imported in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { tasks } from "./data/tasks";
 
-function App() {}
+function App() {
+  console.log(tasks);
 
+  return (
+    <div className="App">
+      <h1>Task Manager</h1>
+    </div>
+  );
+}
 export default App;
